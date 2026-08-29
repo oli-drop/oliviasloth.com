c92f9a42e3dcb5c46c5e73387beb0abdbbbc90ca
Olivia Fjord Sloth <oliviafjordsloth@MacBook-Air-Olivia.local>
2026-08-29 09:55:53 +0200

    Update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ['en', 'da'],
+    defaultLocale: 'en',
+    fallback: {
+      da: 'en',
+    },
+    routing: {
+      prefixDefaultLocale: false,
+      fallbackType: 'rewrite',
+    },
+  },
+});
